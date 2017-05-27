9196c89f8202d2f942d9536c3fa7da1285af3db9
Nate Wright <natew@notthisway.com>
2017-05-27 12:53:39 +0100

    Add a couple of simpler exercises on splitting bills

--- homework/advanced/challenge08.js
+++ homework/advanced/challenge08.js
@@ -1,66 +1,13 @@
-/* Challenge 7: Water bottle
+/* Challenge 8: Paying back a friend
 
-In todays class we learnt about objects and how they are a data type that allows us 
-to store multiple bits of information.
+Nadia, Suzie and Katrina live together. They agreed to buy a kettle and three
+mugs, and split the cost.
 
-Like how a person as an object can have a name or age they also have the ability 
-to perform actions. To do this we can use functions as the values for our keys.
+Nadia went to the shop and bought everything. Write a function to help her
+figure out how much Suzie and Katrina owe her.
 
-For example if we wanted our person object from todays class to speak we would code 
-the following:
+The function should accept the price of a kettle and the price of one of the
+mugs, then return the value Suzie owes.
 
-var person = {
-	firstName: "Joe",
-	lastName: "Bloggs",
-	age: 95,
-	speak: function(){
-		console.log("hello my name is "+ this.firstName);
-	}
-};
-
-You will notice here that when referring to the firstName of our person in another 
-property that also belongs to our person we use the keyword 'this'. 
-This relates to scope in Javascript and becomes more important as your programs 
-become more complicated, we will cover the reasons for this later in the classes 
-but if you would like to read more about the 'this' keyword you can do so here:
-https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Operators/this 
-
-Now we would like you to create a bottle object and give it the ability to be filled, 
-drank from and a function that checks whether it is empty.
-
-To measure this your bottle will need a volume along with fill, drink and empty keys.
-
-We have made a start on this for you here:  
-
-var bottle = {
-	volume: 0,
-	fill: function(){
-	 	// calling this function should make you bottles volume = 100; 
-	},
-	drink: function(){
-		// calling this function should decrease your bottles volume by 10;
-	},
-	empty: function(){
-		// this function should return true if your bottles volumn = 0
-	}
-}
-
---TIP--
-remember 'this' keyword
-
-Once you have completed your object run the following and see if your answer 
-matches up :)
-
-bottle.fill();
-bottle.drink();
-bottle.drink();
-bottle.drink();
-
-if(!bottle.empty()){
-	console.log('bottles volume = ' + bottle.volume);
-}
-
---ANSWER--
-70
-
+Test your function with different prices to see if it works.
 */
