1afa655358c9f8e3867728e98507f51d13b1d824
Bilal CAGIRAN <bcagiran@hotmail.com>
2017-11-05 11:40:31 +0000

    Update challenge-1.js

--- week-3/challenge-1.js
+++ week-3/challenge-1.js
@@ -17,7 +17,7 @@ The index of this number is: 4
 */
 
 const arr = [10, 12, 15, 21, 34];
-for (let i = 0; i < arr.length; i++) {
+for (var i = 0; i < arr.length; i++) {
   setTimeout(function() {
     console.log('The index of this number is: ' + i);
   }, 1000);
